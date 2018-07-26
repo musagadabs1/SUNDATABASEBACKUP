e0dfab008003fb90319076190ca08496724c17b6
Musa Sule <musagadabs1@.com>
2018-07-26 09:25:30 +0100

    Added Icon

--- SUNDBBACKUP/Form1.Designer.cs
+++ SUNDBBACKUP/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnBackup = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtServername = new System.Windows.Forms.TextBox();
@@ -42,6 +43,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.txtCount = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBackup
@@ -49,7 +52,7 @@
             this.btnBackup.BackColor = System.Drawing.Color.MidnightBlue;
             this.btnBackup.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBackup.ForeColor = System.Drawing.Color.White;
-            this.btnBackup.Location = new System.Drawing.Point(88, 244);
+            this.btnBackup.Location = new System.Drawing.Point(94, 280);
             this.btnBackup.Name = "btnBackup";
             this.btnBackup.Size = new System.Drawing.Size(253, 38);
             this.btnBackup.TabIndex = 0;
@@ -61,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Location = new System.Drawing.Point(18, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 18);
             this.label1.TabIndex = 1;
@@ -70,7 +73,7 @@
             // txtServername
             // 
             this.txtServername.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtServername.Location = new System.Drawing.Point(99, 38);
+            this.txtServername.Location = new System.Drawing.Point(105, 74);
             this.txtServername.Name = "txtServername";
             this.txtServername.Size = new System.Drawing.Size(242, 26);
             this.txtServername.TabIndex = 2;
@@ -78,7 +81,7 @@
             // txtusername
             // 
             this.txtusername.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtusername.Location = new System.Drawing.Point(99, 70);
+            this.txtusername.Location = new System.Drawing.Point(105, 106);
             this.txtusername.Name = "txtusername";
             this.txtusername.Size = new System.Drawing.Size(242, 26);
             this.txtusername.TabIndex = 4;
@@ -87,7 +90,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 73);
+            this.label2.Location = new System.Drawing.Point(35, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 18);
             this.label2.TabIndex = 3;
@@ -96,7 +99,7 @@
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(99, 102);
+            this.txtPassword.Location = new System.Drawing.Point(105, 138);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(242, 26);
             this.txtPassword.TabIndex = 6;
@@ -106,7 +109,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(33, 110);
+            this.label3.Location = new System.Drawing.Point(39, 146);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(71, 18);
             this.label3.TabIndex = 5;
@@ -116,7 +119,7 @@
             // 
             this.btnTestConnection.BackColor = System.Drawing.Color.MidnightBlue;
             this.btnTestConnection.ForeColor = System.Drawing.Color.White;
-            this.btnTestConnection.Location = new System.Drawing.Point(220, 134);
+            this.btnTestConnection.Location = new System.Drawing.Point(226, 170);
             this.btnTestConnection.Name = "btnTestConnection";
             this.btnTestConnection.Size = new System.Drawing.Size(121, 38);
             this.btnTestConnection.TabIndex = 7;
@@ -126,7 +129,7 @@
             // chkSaveLogin
             // 
             this.chkSaveLogin.AutoSize = true;
-            this.chkSaveLogin.Location = new System.Drawing.Point(99, 138);
+            this.chkSaveLogin.Location = new System.Drawing.Point(105, 174);
             this.chkSaveLogin.Name = "chkSaveLogin";
             this.chkSaveLogin.Size = new System.Drawing.Size(115, 17);
             this.chkSaveLogin.TabIndex = 8;
@@ -137,7 +140,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(29, 181);
+            this.label4.Location = new System.Drawing.Point(35, 217);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(83, 18);
             this.label4.TabIndex = 9;
@@ -146,7 +149,7 @@
             // txtFolderPath
             // 
             this.txtFolderPath.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFolderPath.Location = new System.Drawing.Point(109, 178);
+            this.txtFolderPath.Location = new System.Drawing.Point(115, 214);
             this.txtFolderPath.Name = "txtFolderPath";
             this.txtFolderPath.Size = new System.Drawing.Size(163, 26);
             this.txtFolderPath.TabIndex = 10;
@@ -155,7 +158,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.MidnightBlue;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(278, 178);
+            this.button1.Location = new System.Drawing.Point(284, 214);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(63, 26);
             this.button1.TabIndex = 11;
@@ -165,7 +168,7 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(88, 210);
+            this.progressBar1.Location = new System.Drawing.Point(94, 246);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(108, 28);
             this.progressBar1.TabIndex = 12;
@@ -174,18 +177,29 @@
             // txtCount
             // 
             this.txtCount.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCount.Location = new System.Drawing.Point(202, 212);
+            this.txtCount.Location = new System.Drawing.Point(208, 248);
             this.txtCount.Name = "txtCount";
             this.txtCount.Size = new System.Drawing.Size(139, 26);
             this.txtCount.TabIndex = 13;
             this.txtCount.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SUNDBBACKUP.Properties.Resources.SkylineLogo1;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(389, 294);
+            this.ClientSize = new System.Drawing.Size(371, 328);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtCount);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
@@ -200,10 +214,12 @@
             this.Controls.Add(this.txtServername);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnBackup);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "SUN | Database Backup Engine";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,6 +241,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
